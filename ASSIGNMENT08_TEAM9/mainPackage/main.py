999e5f3f4b3586d2db2f51c88c8ea09d0e57f37c
conno <conno@DESKTOP-QMODCA8.wifi.uc.edu>
2024-03-26 12:08:48 -0400

    Main

--- ASSIGNMENT08_TEAM9/mainPackage/main.py
+++ ASSIGNMENT08_TEAM9/mainPackage/main.py
@@ -20,4 +20,3 @@ if __name__ == "__main__":
     print("The below employees work in the Amelia store location and are ordered by their total transactions from least to greatest:")
     for LastName, FirstName, EmployeeTransactions in employeeList:
         print("Name:", FirstName + " " + LastName + "; " + "Total Transactions:", EmployeeTransactions)
-
